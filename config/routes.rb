375ff3c9a5c519d4aed419c9f3ae449c31fb2aa6
Stephen Grice <stephengrice@live.com>
2017-02-18 16:26:09 -0500

    Change routes command

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'home#index'
-	get '/page' 'home#index'
+	get 'page', to: 'home#index'
 end
